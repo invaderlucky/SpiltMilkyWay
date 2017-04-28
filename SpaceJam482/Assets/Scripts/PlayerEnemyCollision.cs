ddf304ea29258a77bd82492dcd53ddc9a48d73cf
jscsc <jcstone3@ncsu.edu>
2017-04-28 11:19:12 -0400

    Health Bar for players

---
+++ SpaceJam482/Assets/Scripts/PlayerEnemyCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEnemyCollision : MonoBehaviour {
+
+	public GameObject player;
+	
+	void OnTriggerStay2D(Collider2D other) {
+		player.SendMessage("TakeDamage", 1.0f);
+	}
+	
+}
--- SpaceJam482/Assets/Scripts/PlayerHealth.cs
+++ SpaceJam482/Assets/Scripts/PlayerHealth.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
-    public int startingHealth = 100;                            // The amount of health the player starts the game with.
-    public int currentHealth;                                   // The current health the player has.
+	public float maxHealth = 100f;
+    public float startingHealth = 100f;                            // The amount of health the player starts the game with.
+    public float currentHealth;                                   // The current health the player has.
     public Slider healthSlider;                                 // Reference to the UI's health bar.
     public Image damageImage;                                   // Reference to an image to flash on the screen on being hurt.
     public AudioClip deathClip;                                 // The audio clip to play when the player dies.
     public float flashSpeed = 5f;                               // The speed the damageImage will fade at.
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);     // The colour the damageImage is set to, to flash.
+
+    public GameObject HealthBar;
 
 
     //Animator anim;                                              // Reference to the Animator component.
@@ -40,13 +43,13 @@ public class PlayerHealth : MonoBehaviour {
         if(damaged)
         {
             // ... set the colour of the damageImage to the flash colour.
-            damageImage.color = flashColour;
+            //damageImage.color = flashColour;
         }
         // Otherwise...
         else
         {
             // ... transition the colour back to clear.
-            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            //damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
         }
 
         // Reset the damaged flag.
@@ -54,7 +57,7 @@ public class PlayerHealth : MonoBehaviour {
     }
 
 
-    public void TakeDamage (int amount)
+    public void TakeDamage (float amount)
     {
         // Set the damaged flag so the screen will flash.
         damaged = true;
@@ -63,10 +66,14 @@ public class PlayerHealth : MonoBehaviour {
         currentHealth -= amount;
 
         // Set the health bar's value to the current health.
-        healthSlider.value = currentHealth;
+        //healthSlider.value = currentHealth;
+        float calcHealth = currentHealth / maxHealth; // so like 80 / 100 = 0.8f
+        if (calcHealth < 0)
+        	calcHealth = 0;
+        setHealthBar(calcHealth);
 
         // Play the hurt sound effect.
-        playerAudio.Play ();
+        //playerAudio.Play ();
 
         // If the player has lost all it's health and the death flag hasn't been set yet...
         if(currentHealth <= 0 && !isDead)
@@ -74,6 +81,11 @@ public class PlayerHealth : MonoBehaviour {
             // ... it should die.
             Death ();
         }
+    }
+
+    public void setHealthBar(float newHealth) {
+    	// newHealth needs to be a value between 0 and 1, 
+		HealthBar.transform.localScale = new Vector3(newHealth, HealthBar.transform.localScale.y, HealthBar.transform.localScale.z);
     }
 
 
@@ -89,8 +101,8 @@ public class PlayerHealth : MonoBehaviour {
         anim.SetTrigger ("Die");
  */
         // Set the audiosource to play the death clip and play it (this will stop the hurt sound from playing).
-        playerAudio.clip = deathClip;
-        playerAudio.Play ();
+        //playerAudio.clip = deathClip;
+        //playerAudio.Play ();
 
         // Turn off the movement and shooting scripts.
         //playerMovement.enabled = false;
